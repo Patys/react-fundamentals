10dfbd49f369a03f9c8313db67543e524d33b38b
Patryk Szczygło <patysprog@gmail.com>
2017-07-03 16:14:29 +0200

    added title widget

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ class App extends React.Component {
     let txt = this.props.txt;
     return (
       <div>
+        <TitleWidget text="Nauka react'a"/>
         <h1>Hi :)</h1>
         <b>Bold</b>
         <p>{txt}</p>
@@ -31,5 +32,17 @@ class App extends React.Component {
 
 const InputWidget = (props) => <input type="text" onChange={props.update}/>
 const ButtonWidget = (props) => <button>{props.children}</button>
+const TitleWidget = (props) => <h1>Title: {props.text}</h1>
+
+TitleWidget.propTypes = {
+  text(props, propName, component) {
+    if(!(propName in props)) {
+      return new Error(`missing ${propName}`);
+    }
+    if(props[propName].length < 6) {
+      return new Error(`${propName} was too short`);
+    }
+  }
+}
 
 export default App
