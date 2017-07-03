55d70b613c1f89155457dc74150efc3172089c37
Patryk Szczygło <patysprog@gmail.com>
2017-07-03 15:56:20 +0200

    added prop

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
+    let txt = this.props.txt;
     return (
       <div>
         <h1>Hi :)</h1>
         <b>Bold</b>
+        <p>{txt}</p>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -3,5 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <App txt="this is the prop text"/>,
+  document.getElementById('root')
+);
 registerServiceWorker();
