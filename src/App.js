9d94f3fc0c0adb45e775407fc21a5fdc8145df74
Patryk Szczygło <patysprog@gmail.com>
2017-07-03 16:02:03 +0200

    added input element with update function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      myTxt: 'Patryk wita świat :D'
+    }
+  }
+
+  update(e) {
+    this.setState({myTxt: e.target.value});
+  }
+
   render() {
     let txt = this.props.txt;
     return (
@@ -8,6 +19,8 @@ class App extends React.Component {
         <h1>Hi :)</h1>
         <b>Bold</b>
         <p>{txt}</p>
+        <input type="text" onChange={this.update.bind(this)}/>
+        <p>{this.state.myTxt}</p>
       </div>
     )
   }
